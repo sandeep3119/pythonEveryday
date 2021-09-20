80de4989b5ddbb3a0d595b41a96b0441e42ede9c
Sandeep Kumar Rana <rana.sandeep3119@gmail.com>
2021-09-20 20:26:54 +0530

    Readme.md updated

--- File-Segregator.py
+++ File-Segregator.py
@@ -1,18 +1,21 @@
 import os
 import shutil 
-
-print(os.getcwd())
-
-data = os.listdir(".")
-
-for file_ in  data:
-    # print(file_)
-    dir_name=file_.split(".")[-1]
-    os.makedirs(dir_name,exist_ok=True)
-    if (os.path.isfile(file_) and file_!='script.py') :
-        src_file_path=file_
-        dest_file_path=os.path.join(dir_name,file_)
-        shutil.move(src_file_path,dest_file_path)
+import argparse
 
 
+def main(TARGET_DIR):
+    os.chdir(TARGET_DIR)
+    data = os.listdir(".")
+    for file_ in  data:
+        dir_name=file_.split(".")[-1]
+        os.makedirs(dir_name,exist_ok=True)
+        if (os.path.isfile(file_) and file_!='script.py') :
+            src_file_path=file_
+            dest_file_path=os.path.join(dir_name,file_)
+            shutil.move(src_file_path,dest_file_path)
+
+if __name__=='__main__':
+    args=argparse.ArgumentParser()
+    parsed_args=args.parse_args('--target',default=os.getcwd())
+    main(parsed_args.target)
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pythonEveryday
-Small scripts to automate daya to day tasks
+Small scripts to automate day to day tasks using python
+
+### File-Segregator.py
+Segregates files based upon extensions and move to different folders.
+```bash
+python FileSegregator.py --target=YOUR_TARGET_FOLDER
+```
+* By default --target=Current working directory
+
+### Weeding-Script.py
+Moves all non-image/corrupt files to the specified Trash Folder.
+```bash
+python Weeding-Script.py --target=YOUR_TARGET_FOLDER
+```
+* By default --target=Current working directory
+
